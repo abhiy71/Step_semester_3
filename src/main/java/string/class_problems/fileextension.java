9687539aec020a545530d80718c3ddfe46053002
abhiy71 <abhinavsyadav2006@gmail.com>
2026-09-05 23:31:59 +0530

    Add session 2 practice problems

---
+++ src/main/java/string/class_problems/fileextension.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Main {
+    static void parseStudentRecord(String csvLine) {
+        String[] data = csvLine.split(",");
+        if (data.length != 3) {
+            System.out.println("Invalid Record");
+        } else {
+            System.out.println("Name: " + data[0] +
+                    " | Roll No: " + data[1] +
+                    " | Dept: " + data[2]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter student record: ");
+        String record = sc.nextLine();
+        parseStudentRecord(record);
+        sc.close();
+    }
+}
